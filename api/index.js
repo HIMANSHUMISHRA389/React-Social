3d12622ddc463f09d3c320c40a669e9943b4911e
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2024-02-20 22:50:34 +0530

    api for social-app

---
+++ api/index.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const app = express();
+const helmet = require("helmet");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const { default: mongoose } = require("mongoose");
+const userRouter=require('./routes/users')
+const authRoute=require('./routes/auth')
+dotenv.config();
+const uri = process.env.MONGO_URL;
+
+//middlewares
+app.use(express.json())
+app.use(helmet())
+app.use(morgan("common"))
+
+
+//APIs
+app.use("/api/users", userRouter);
+app.use("/api/auth", authRoute);
+
+
+
+mongoose
+  .connect(uri, { })
+  .then(() => console.log("Connected to MongoDB Atlas"))
+  .catch((err) => console.error("Error connecting to MongoDB Atlas:", err));
+
+app.listen(8000,() => {
+  console.log("Server started at loacalhost:8000");
+});
