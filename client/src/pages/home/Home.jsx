bb43812837e2f130a58139fc643c19bddba30d21
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2024-03-23 00:19:18 +0530

    y

--- client/src/pages/home/Home.jsx
+++ client/src/pages/home/Home.jsx
@@ -1,4 +1,4 @@
-import Topbar from "../../components/topbar/Topbar";
+// import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
@@ -7,7 +7,7 @@ import "./home.css"
 export default function Home() {
   return (
     <>
-      <Topbar />
+      {/* <Topbar /> */}
       <div className="homeContainer">
         <Sidebar />
         <Feed/>
