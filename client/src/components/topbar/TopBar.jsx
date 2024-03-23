52bcaea732a29f5649b9f5596d405b2fdfff6057
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2024-03-23 22:24:11 +0530

    -

--- client/src/components/topbar/TopBar.jsx
+++ client/src/components/topbar/TopBar.jsx
@@ -15,7 +15,7 @@ const chat=()=>{
     <div className="topbarContainer">
       <div className="topbarLeft">
         <Link to="/" style={{ textDecoration: "none" }}>
-          <span className="logo">Lamasocial</span>
+          <span className="logo">Facebook</span>
         </Link>
       </div>
       <div className="topbarCenter">
