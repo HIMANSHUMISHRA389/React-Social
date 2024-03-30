85a48c3571782339daf5285e288bcd1fd2ef8102
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2024-03-30 17:12:30 +0530

    -

--- api/routes/conversation.js
+++ api/routes/conversation.js
@@ -1,30 +1,15 @@
 const router = require("express").Router();
-const Conversation = require("../models/Conversation.model");
 
-//post a message
-router.post("/", async (req, res) => {
+
+
+
+//get message
+router.get("/", (req, res) => {
   try {
-    const msg = new Conversation({
-      members: [req.body.senderId, req.body.receiverId],
-    });
-    const savedMsg = await msg.save();
-    res.status(200).send(savedMsg);
+    res.status(200).send("its working fine");
   } catch (error) {
-    res.send(404).send(error);
+    console.log(error);
   }
 });
 
-//get a conversation based on userId
-router.get("/:userId", async (req, res) => {
-  try {
-    const search = req.params.userId;
-    console.log(search);
-    const conve = await Conversation.find({
-      members: { $regex: search, $options: "i" },
-    });
-    res.status(200).send(conve);
-  } catch (error) {
-    res.send(404).send(error);
-  }
-});
 module.exports = router;
