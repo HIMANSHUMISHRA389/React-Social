d4444afbc3dbeec3e47aa00ac5adbf69f86ed8d9
HIMANSHUMISHRA389 <himanshumishra389@gmail.com>
2024-03-30 16:17:47 +0530

    -

--- api/routes/conversation.js
+++ api/routes/conversation.js
@@ -1,15 +1,14 @@
 const router = require("express").Router();
 const Conversation = require("../models/Conversation");
 
-
 //post a message
-router.post("/",  async(req, res) => {
+router.post("/", async (req, res) => {
   try {
     const msg = new Conversation({
       members: [req.body.senderId, req.body.receiverId],
     });
-    const savedMsg = await msg.save()
-   res.status(200).send(savedMsg)
+    const savedMsg = await msg.save();
+    res.status(200).send(savedMsg);
   } catch (error) {
     res.send(404).send(error);
   }
@@ -18,13 +17,14 @@ router.post("/",  async(req, res) => {
 //get a conversation based on userId
 router.get("/:userId", async (req, res) => {
   try {
-    const search=req.params.userId
-    console.log(search)
-    const conve=await Conversation.find({ members: { $regex: search, $options: 'i' } })
+    const search = req.params.userId;
+    console.log(search);
+    const conve = await Conversation.find({
+      members: { $regex: search, $options: "i" },
+    });
     res.status(200).send(conve);
   } catch (error) {
-   res.send(404).send(error)
+    res.send(404).send(error);
   }
 });
-module.exports=router
-
+module.exports = router;
